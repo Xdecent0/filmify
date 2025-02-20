6e0cc219d89b37d4517a61dae5832fc04bdf5bea
stapxd <foxionhk@gmail.com>
2025-02-20 18:58:17 +0200

    fix: adjusted z-index in menu

--- js/header.js
+++ js/header.js
@@ -3,7 +3,14 @@ const buttonImage = menuButton.querySelector('.header__menu-button__icon');
 const menuBackground = document.querySelector('.menu__background');
 const menu = document.querySelector('.header__menu');
 
-menuButton.addEventListener('click', function(){ 
+initMenu()
+
+function initMenu(){ 
+    menuButton.addEventListener('click', toggleMenu);
+    menuBackground.addEventListener('click', toggleMenu);
+}
+
+function toggleMenu(){
     let imageFile = buttonImage.getAttribute('src');
 
     menuButton.classList.toggle('button--fixed');
@@ -13,24 +20,19 @@ menuButton.addEventListener('click', function(){
     
     if(menu.classList.toggle('header__menu--active')){ 
         imageFile = imageFile.replace(/\w+.{1}\w+$/, 'cross.svg'); 
+
+        menuBackground.style.zIndex = 3;
+        menuButton.style.zIndex = 5;
+        menu.style.zIndex = 4;
+
     }
-    else { imageFile = imageFile.replace(/\w+.{1}\w+$/, 'menu.svg'); }
+    else { 
+        imageFile = imageFile.replace(/\w+.{1}\w+$/, 'menu.svg'); 
+
+        menuBackground.style.zIndex = 1;
+        menuButton.style.zIndex = 1;
+        menu.style.zIndex = 1;
+    }
         
     buttonImage.setAttribute('src', imageFile);
-});
-
-menuBackground.addEventListener('click', function(){ 
-    let imageFile = buttonImage.getAttribute('src');
-
-    menuButton.classList.toggle('button--fixed');
-    menuButton.classList.toggle('z-index--max');
-
-    menuBackground.classList.toggle('visually-hidden');
-    
-    if(menu.classList.toggle('header__menu--active')){ 
-        imageFile = imageFile.replace(/\w+.{1}\w+$/, 'cross.svg'); 
-    }
-    else { imageFile = imageFile.replace(/\w+.{1}\w+$/, 'menu.svg'); }
-        
-    buttonImage.setAttribute('src', imageFile);
-});
+}
